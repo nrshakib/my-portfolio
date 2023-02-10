61f80fe08fa651adb4b653a28f5ae25fb81e44e0
NRS\nrsha <nrshakib007@gmail.com>
2023-02-10 13:40:31 +0600

    footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Footer.css';
+import { BsFacebook, BsLinkedin, BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+        <footer>
+            <a href="#" className='footer-logo'>MD Nadim Rijvi Shakib</a>
+
+            <div className="footer-socials">
+                <a href="https://www.facebook.com/nadimrijvi.shakib"><BsFacebook /></a>
+                <a href="https://www.linkedin.com/in/mdnadimrijvishakib/"><BsLinkedin /></a>
+                <a href="https://github.com/nrshakib"><BsGithub /></a>
+            </div>
+            <div className="footer-copyright">
+                <small>&copy; MD Nadim Rijvi Shakib.
+                    <p>All Right Reserved</p>
+                    <p>{new Date().getFullYear()}</p>
+                </small>
+            </div>
+        </footer>
     );
 };
 
--- src/components/header/HeaderSocial.js
+++ src/components/header/HeaderSocial.js
@@ -4,8 +4,8 @@ import { BsGithub, BsLinkedin, BsDoorOpenFill } from 'react-icons/bs';
 const HeaderSocial = () => {
     return (
         <div className='header-socials'>
-            <a href="https://linkedin.com" target='_blank'><BsLinkedin></BsLinkedin></a>
-            <a href="https://github.com" target='_blank'><BsGithub></BsGithub></a>
+            <a href="https://www.linkedin.com/in/mdnadimrijvishakib/" target='_blank'><BsLinkedin></BsLinkedin></a>
+            <a href="https://github.com/nrshakib" target='_blank'><BsGithub></BsGithub></a>
             <a href="https://bdjobs.com" target='_blank'><BsDoorOpenFill /></a>
         </div>
     );
