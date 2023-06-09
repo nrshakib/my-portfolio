f16ad529013dfef61d4f1c12fbc6d208604b1f69
NRS\nrsha <nrshakib007@gmail.com>
2023-06-09 23:52:20 +0600

    Updated CV

--- src/components/header/action.js
+++ src/components/header/action.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import CV from '../../resources/files/CV_MD_Nadim_Rijvi_Shakib.pdf';
+import React from "react";
+import CV from "../../resources/files/CV_MD_Nadim_Rijvi_Shakib.pdf";
 
 const Action = () => {
-    return (
-        <div className='action'>
-            <a href={CV} download className='btn'>Download CV</a>
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-        </div>
-    );
+  return (
+    <div className="action">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Have a Talk
+      </a>
+    </div>
+  );
 };
 
 export default Action;
