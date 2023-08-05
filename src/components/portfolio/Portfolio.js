203d7244c6729653d6b56cb70e8cb9183debaaec
NRS\nrsha <nrshakib007@gmail.com>
2023-08-06 01:07:58 +0600

    updated works in portfolio section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,36 +1,52 @@
 import React from "react";
 import "./Portfolio.css";
-import Img1 from "../../resources/images/portfolio/admin-dashboard.jpg";
-import Img2 from "../../resources/images/portfolio/restaurant.png";
-import Img3 from "../../resources/images/portfolio/e-commerce.jpg";
-import Img4 from "../../resources/images/portfolio/temperature app.jpg";
+import Img1 from "../../resources/images/portfolio/car-service.jpg";
+import Img2 from "../../resources/images/portfolio/ecommerce-ej.jpg";
+import Img3 from "../../resources/images/portfolio/admin-dashboard.jpg";
+import Img4 from "../../resources/images/portfolio/restaurant.png";
+import Img5 from "../../resources/images/portfolio/e-commerce.jpg";
+import Img6 from "../../resources/images/portfolio/temperature app.jpg";
 
 const data = [
   {
     id: 1,
     image: Img1,
+    title: "Car Service Site",
+    github: "https://github.com/nrshakib/genius-car-services",
+    live: "https://github.com/nrshakib/genius-car-services",
+  },
+  {
+    id: 2,
+    image: Img2,
+    title: "E-commerce Site",
+    github: "https://github.com/nrshakib/ecommerce-ej-fullstack",
+    live: "https://github.com/nrshakib/ecommerce-ej-fullstack",
+  },
+  {
+    id: 3,
+    image: Img3,
     title: "Admin Dashboard using ReactJs",
     github: "https://github.com/nrshakib/admin-dashboard",
     live: "https://nrshakib.github.io/admin-dashboard/",
   },
   {
-    id: 2,
-    image: Img2,
+    id: 4,
+    image: Img4,
     title: "Restaurant Website using ReactJs",
     github: "https://github.com/nrshakib/restaurant-website-react",
     live: "https://nrshakib.github.io/restaurant-website-react/",
   },
   {
-    id: 3,
-    image: Img3,
+    id: 5,
+    image: Img5,
     title: "E-Commerce Site using JS",
     github:
       "https://github.com/nrshakib/Panda-eCommerce-usingBootstrap-from-Figma",
     live: "https://nrshakib.github.io/Panda-eCommerce-usingBootstrap-from-Figma/",
   },
   {
-    id: 4,
-    image: Img4,
+    id: 6,
+    image: Img6,
     title: "City Temperature App using Js",
     github: "https://github.com/nrshakib/city-temperature",
     live: "https://nrshakib.github.io/city-temperature/",
