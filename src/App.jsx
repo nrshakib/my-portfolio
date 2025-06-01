4bc122f4ac49a43190dc4ab4a17e4f2ba5de472d
nrshakib <nrshakib007@gmail.com>
2025-06-01 14:52:19 +0600

    Upgrated to React Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Navbar from './components/navbar/Navbar';
-import Portfolio from './components/portfolio/Portfolio';
-import Services from './components/services/Services';
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 function App() {
   return (
-    <div className="App">
+    <div className="pt-10">
       <Header></Header>
       <Navbar></Navbar>
       <About></About>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,34 @@
-import React from "react";
 import Action from "./action";
-import "./Header.css";
 import Me from "../../../public/resources/images/me-4.png";
 import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header-container">
-        <h5>Hello I'm</h5>
+    <header className="pt-10 relative h-auto md:h-[68vh] sm:h-[120vh]">
+      <div className="text-center h-full relative">
+        <p className="text-2xl">Hello I'm</p>
         <h1>MD Nadim Rijvi Shakib</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
-        <Action />
-        <HeaderSocial></HeaderSocial>
+        <h5 className="text-gray-400">Full Stack Developer</h5>
 
-        <div className="me">
-          <img src={Me} alt="" />
+        <div className="mt-10 flex gap-5 justify-center">
+          <Action />
         </div>
-        <a href="#contact" className="scroll-down">
+
+        <div className="header-socials flex flex-col items-center gap-2 absolute left-0 bottom-12 before:content-[''] before:block before:w-px before:h-8 before:bg-primary">
+          <HeaderSocial />
+        </div>
+
+        <div
+          className="me bg-gradient-to-t from-primary to-transparent w-[22rem] h-[21.8rem] absolute left-1/2 -translate-x-1/2 mt-16 rounded-t-[12rem] overflow-hidden p-8"
+          style={{ paddingRight: '1.5rem', paddingLeft: '1.5rem', paddingBottom: '1.5rem' }}
+        >
+          <img src={Me} alt="Me" />
+        </div>
+
+        <a
+          href="#contact"
+          className="scroll-down absolute bottom-20 -right-9 rotate-90 font-light text-sm"
+        >
           Scroll Down
         </a>
       </div>
